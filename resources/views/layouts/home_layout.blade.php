18c7d3f629ac2cf145df417f5b6c61ad60ccf35b
Shannon Francis Retuerma <snretuerma@up.edu.ph>
2019-06-20 13:45:51 +0800

    Fix google verification

--- resources/views/layouts/home_layout.blade.php
+++ resources/views/layouts/home_layout.blade.php
@@ -2,6 +2,7 @@
 <html lang="en">
 <meta name="csrf-token" content="{{ csrf_token() }}">
 <head>
+    <meta name="google-site-verification" content="RaHF0nFyoPExG_VIxLw1CJEMPUiW8xA0mUEEldLWCsg" />
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
